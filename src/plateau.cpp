63369f313b6765fff9db0dd0bb417195659588b4
Clement <clement.bernard.pro130@gmail.com>
2024-10-15 09:17:43 +0200

    création du plateau

---
+++ src/plateau.cpp
@@ -0,0 +1,155 @@
+#include "plateau.h"
+#include "ihm.h"
+#include "données.h"
+#include <ctime> // pour time
+#include <cstdlib> // pour srand et rand
+#include <algorithm> // pour any_of
+
+#ifdef DEBUG_JEU
+#include <iostream>
+#endif
+
+int lancerDes(int desObtenue[NB_DES], int desEnJeu) {
+    srand(time(NULL));
+#ifdef DEBUG_JEU
+    std::cout << "[" << __FILE__ << ":" << __PRETTY_FUNCTION__ << ":" << __LINE__ << "] ";
+    std::cout << "desEnJeu = " << desEnJeu << std::endl;
+#endif
+
+    for (int i = 0; i < desEnJeu; i++) {
+        int valeur = (rand() % NB_FACES) + 1;
+        desObtenue[i] = valeur;
+    }
+    return 0;
+}
+
+int convertirValeur(char valeur) {
+    if (valeur == 'V' || valeur == 'v')
+        return 6;
+    else if (valeur >= '1' && valeur <= '5')
+        return valeur - '0';
+    return -1;
+
+}
+
+bool contientV(int desGarder[NB_DES]) {
+    for (int i = 0; i < NB_DES; i++) {
+        if (desGarder[i] == 6)
+            return true;
+    }
+    return false;
+}
+
+bool sontEgal(int des1[NB_DES], int des2[NB_DES]) {
+    for (size_t i = 0; i < NB_DES; i++) {
+        if (des1[i] != des2[i])
+            return false;
+    }
+    return true;
+}
+
+bool peutRelancer(int desObtenue[NB_DES], int desGarder[NB_DES], int desEnJeu) {
+    if (sontEgal(desObtenue, desGarder))
+        return false;
+
+    for (int i = 0; i < desEnJeu; i++) {
+        bool dejaGardee = std::any_of(desGarder, desGarder + NB_DES, [desObtenue, i](int v) { return v == desObtenue[i]; });
+        if (!dejaGardee)
+            return true;
+    }
+    return false;
+}
+
+bool rechercherEtGarder(int desGarder[NB_DES],
+                        int desObtenue[NB_DES],
+                        int valeurGardee,
+                        int desEnJeu)
+{
+    bool gardeEffectuee = false;
+    for (int i = 0; i < desEnJeu; i++) {
+        if (desObtenue[i] == valeurGardee) {
+            for (size_t j = 0; j < NB_DES; j++) {
+                if (desGarder[j] == 0) {
+                    desGarder[j] = valeurGardee;
+                    gardeEffectuee = true;
+                    break;
+                }
+            }
+        }
+    }
+    return gardeEffectuee;
+}
+
+void garderDES(int desGarder[NB_DES], int desObtenue[NB_DES], int desEnJeu) {
+    int valeurGardee;
+    bool dejaGardee;
+    do {
+        valeurGardee = convertirValeur(demanderValeur());
+        if (valeurGardee == -1) { afficherErreurEntree(); return; }
+
+        dejaGardee = std::any_of(desGarder, desGarder + NB_DES, [valeurGardee](int v) { return v == valeurGardee; });
+        if (dejaGardee) afficherValeurDejaGardee();
+
+    } while (dejaGardee);
+
+    rechercherEtGarder(desGarder, desObtenue, valeurGardee, desEnJeu);
+    afficherDesGardes(desGarder, NB_DES);
+}
+
+bool verifierNombreDesGarder(int desGarder[NB_DES], int& desEnJeu) {
+    int compteurDesGardes = 0;
+    for (int i = 0; i < NB_DES; i++) {
+        if (desGarder[i] != 0) {
+            compteurDesGardes++;
+        }
+    }
+    if (compteurDesGardes == NB_DES) {
+        afficherTousDesGardes();
+        return false;
+    } else {
+        desEnJeu = NB_DES - compteurDesGardes;
+        return true;
+    }
+}
+
+int jouerTour(int desGarder[NB_DES], int& desEnJeu) {
+    int desObtenue[NB_DES];
+    lancerDes(desObtenue, desEnJeu);
+    afficherDes(desObtenue, desEnJeu); 
+
+    if (!peutRelancer(desObtenue, desGarder, desEnJeu)) {
+        afficherJeuNul();
+        return 0;
+    }
+
+    garderDES(desGarder, desObtenue, desEnJeu);
+    return 1;
+}
+
+int obtenirSuiteDeDes() {
+    int desGarder[NB_DES] = { 0 };
+    int desEnJeu = NB_DES;
+
+    while (verifierNombreDesGarder(desGarder, desEnJeu)) {
+        if (jouerTour(desGarder, desEnJeu) == 0) {
+            return 0;
+        }
+
+        if (!demanderContinuer()) {
+            break;
+        }
+    }
+
+    if (!contientV(desGarder)) {
+        afficherJeuNul();
+        return 0;
+    }
+
+#ifdef DEBUG_JEU
+    std::cout << "[" << __FILE__ << ":" << __PRETTY_FUNCTION__ << ":" << __LINE__ << "] ";
+    std::cout << "Le joueur peut piocher un pickomino " << std::endl;
+#endif
+
+    return 0;
+}
+
---
+++ src/plateau.h
@@ -0,0 +1,19 @@
+#ifndef JEU_H
+#define JEU_H
+
+#define DEBUG_JEU
+
+#include "données.h"
+
+int  obtenirSuiteDeDes();
+int  demanderValeur();
+bool rechercherEtGarder(int desGarder[NB_DES],
+                        int desObtenue[NB_DES],
+                        int valeurGardee,
+                        int desEnJeu);
+void garderDES(int desGarder[NB_DES], int desObtenue[NB_DES], int desEnJeu);
+bool verifierNombreDesGarder(int desGarder[NB_DES]);
+bool verifierLancer(int desGarder[NB_DES], int& desEnJeu);
+// int score();
+
+#endif
