39e5cf217d9957366f970b9c19baf00e31e57554
clementBernard130 <clement.bernard.pro130@gmail.com>
2024-10-16 08:36:55 +0200

    Restructuration du project et ajout des données généralisées

---
+++ src/donnees.h
@@ -0,0 +1,12 @@
+#ifndef DONNEES_H
+#define DONNEES_H
+
+#define NB_FACES      6
+#define NB_DES        8
+#define NB_JOUEUR_MAX 7
+#define NB_JOUEUR_MIN 2
+#define NB_PICKOMINOS 16
+#define ID_VERS       6
+#define VALEUR_VERS   5
+
+#endif
