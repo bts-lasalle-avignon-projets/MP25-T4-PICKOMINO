1de64d4e4fb35f4acfe5dc57ac35df47eab2a532
ValentinBOUSQUET <valentin.solfrini.pro@gmail.com>
2024-11-08 11:22:25 +0100

    Création de la fonction "creerLesPilesDesJoueurs"

--- src/joueur.h
+++ src/joueur.h
@@ -1,6 +1,16 @@
 #ifndef JOUEUR_H
 #define JOUEUR_H
+#include "donnees.h"
 
 #define DEBUG_JOUEUR
+
+struct Joueur
+{
+    char nomDuJoueur;
+    int  pileJoueur[NB_PICKOMINOS];
+    int  compteur;
+};
+
+void creerLesPilesDesJoueurs(Joueur joueurs[NB_JOUEUR_MAX], int nbJoueurs);
 
 #endif
