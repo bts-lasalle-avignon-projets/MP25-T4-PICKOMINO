4d91de4777fb3fbc953ceab1bede0e35a68893f3
Clement <clement.bernard.pro130@gmail.com>
2024-11-28 20:10:57 +0100

    ajout de la fonction retournerPickominos

--- src/plateau.cpp
+++ src/plateau.cpp
@@ -259,3 +259,25 @@ void retournerPickominos(Plateau& plateau)
         plateau.pickominos[index] = EtatPickomino::RETOURNE; // On retourne ce Pickomino
     }
 }
+void retournerPickominos(Plateau& plateau)
+{
+    int valeur = VALEUR_DE_INCONNUE;
+    int index  = VALEUR_DE_INCONNUE;
+
+    for(int i = 0; i < NB_PICKOMINOS; i++)
+    {
+        if(plateau.pickominos[i] == EtatPickomino::DISPONIBLE)
+        {
+            int valeurPickomino = i + VALEUR_PICKOMINO_MIN;
+            if(valeur == VALEUR_DE_INCONNUE || valeurPickomino > valeur)
+            {
+                valeur = valeurPickomino;
+                index  = i;
+            }
+        }
+    }
+    if(index != VALEUR_DE_INCONNUE)
+    {
+        plateau.pickominos[index] = EtatPickomino::RETOURNE; // On retourne ce Pickomino
+    }
+}
