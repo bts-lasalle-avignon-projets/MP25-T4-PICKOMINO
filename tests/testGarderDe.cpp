346ba95e02bf9a67a8e06bfa2a3759203eecb5ee
clementBernard130 <clement.bernard.pro130@gmail.com>
2025-01-18 18:18:30 +0100

    Test pour garder un vers

--- tests/testGarderDe.cpp
+++ tests/testGarderDe.cpp
@@ -9,13 +9,12 @@ std::istringstream simulatedInput;
 std::ostringstream simulatedOutput;
 
 // Fonction pour simuler l'entrée d'un entier
-void simulerEntree(int nombreSimule)
+void simulerEntree(const std::string& entreeSimulee)
 {
-    simulatedInput.str(""); // Réinitialise le flux simulé
-    simulatedInput.clear(); // Efface l'état du flux
-    simulatedInput.str(
-      std::to_string(nombreSimule)); // Convertir l'entier en chaîne et réinjecter dans le flux
-    std::cin.rdbuf(simulatedInput.rdbuf()); // Rediriger std::cin vers simulatedInput
+    simulatedInput.str("");                 // Réinitialise le flux simulé
+    simulatedInput.clear();                 // Efface l'état du flux
+    simulatedInput.str(entreeSimulee);      // Injecte la chaîne simulée dans le flux
+    std::cin.rdbuf(simulatedInput.rdbuf()); // Redirige std::cin vers simulatedInput
 }
 
 TEST_CASE("Garder un dé valide")
@@ -33,7 +32,7 @@ TEST_CASE("Garder un dé valide")
         plateau.desGardes[i] = 0; // Aucun dé gardé au départ
     }
 
-    simulerEntree(3); // Simuler la saisie de l'entier 3
+    simulerEntree("3\n"); // Simuler la saisie de l'entier 3
 
     // Essayer de garder la valeur 3, présente parmi les dés obtenus
     CHECK(garderDes(plateau) == true);
@@ -91,5 +90,34 @@ TEST_CASE("Le dé est déjà garder")
     for(int i = 0; i < NB_DES - plateau.desEnJeu; i++)
     {
         CHECK(plateau.desGardes[i] == i + 1); // Aucun dé gardé suplémentaire ou modifier
+    }
+}
+
+TEST_CASE("Garder un ver :")
+{
+    Plateau plateau;
+    plateau.desEnJeu      = 8;
+    plateau.desObtenus[0] = 1;
+    plateau.desObtenus[1] = 2;
+    plateau.desObtenus[2] = 3;
+    plateau.desObtenus[3] = 4;
+    plateau.desObtenus[4] = 5;
+    plateau.desObtenus[5] = 5;
+    plateau.desObtenus[6] = ID_VERS;
+    plateau.desObtenus[7] = 5;
+    for(int i = 0; i < NB_DES; i++)
+    {
+        plateau.desGardes[i] = 0; // Aucun dé gardé au départ
+    }
+
+    simulerEntree("v\n");
+
+    bool resultat = garderDes(plateau);
+    CHECK(resultat == true);
+    CHECK(plateau.desEnJeu == 7);
+    CHECK(plateau.desGardes[0] == ID_VERS);
+    for(int i = 1; i < NB_DES; i++)
+    {
+        CHECK(plateau.desGardes[i] == 0); // Aucun dé gardé supplémentaire
     }
 }
